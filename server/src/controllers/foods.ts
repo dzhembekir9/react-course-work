bac9611cd74890769d1d324610ea1f4f42222af1
Dzhem Bekir <dzhem.bekir@gmail.com>
2024-03-12 16:42:37 +0200

    wrap up the api

--- server/src/controllers/foods.ts
+++ server/src/controllers/foods.ts
@@ -1,30 +1,7 @@
 import express from 'express'
-import { Op } from 'sequelize'
 import { Food } from '../models/Food'
 
 export const foodsRouter = express.Router()
-
-foodsRouter.get('/search', async (req, res) => {
-  const description = req.query.desc
-
-  if (description) {
-    const foods = await Food().findAll({
-      where: {
-        description: {
-          [Op.like]: `%${description}%`,
-        },
-      },
-    })
-
-    if (foods.length > 0) {
-      res.status(200).json(foods)
-    } else {
-      res.status(404).json({ message: 'No foods found!' })
-    }
-  } else {
-    res.status(400).json({ message: 'Invalid search query!' })
-  }
-})
 
 foodsRouter.get('/', async (req, res) => {
   try {
@@ -43,39 +20,5 @@ foodsRouter.post('/', async (req, res) => {
     res.status(201).json({ message: 'Food created successfully!' })
   } catch (error) {
     res.status(500).json({ message: 'Error creating food!' })
-  }
-})
-
-foodsRouter.get('/:id', async (req, res) => {
-  const food = await Food().findByPk(req.params.id)
-
-  if (food) {
-    res.status(200).json(food)
-  } else {
-    console.log('IVAN')
-
-    res.status(404).json({ message: 'Food not found!' })
-  }
-})
-
-foodsRouter.delete('/:id', async (req, res) => {
-  const deletedRows = await Food().destroy({ where: { id: req.params.id } })
-
-  if (deletedRows > 0) {
-    res.status(200).json({ message: 'Food deleted successfully!' })
-  } else {
-    res.status(404).json({ message: 'Food not found!' })
-  }
-})
-
-foodsRouter.put('/:id', async (req, res) => {
-  const updated = await Food().update(req.body, {
-    where: { id: req.params.id },
-  })
-
-  if (updated[0] > 0) {
-    res.status(200).json({ message: 'Food updated successfully!' })
-  } else {
-    res.status(404).json({ message: 'Food not found!' })
   }
 })
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import { initializeDB, syncDatabase } from './dbConfig'
 import { foodsRouter } from './controllers/foods'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use('/foods', foodsRouter)
 
 const PORT = process.env.PORT || 8080

7dd89ff15c770b4438cf1c3699381a5af8696b8c
Dzhem Bekir <dzhem.bekir@gmail.com>
2024-03-26 20:06:45 +0200

    add search endpoint

--- server/src/controllers/foods.ts
+++ server/src/controllers/foods.ts
@@ -1,7 +1,24 @@
 import express from 'express'
+import { Op } from 'sequelize'
 import { Food } from '../models/Food'
 
 export const foodsRouter = express.Router()
+
+foodsRouter.get('/search', async (req, res) => {
+  try {
+    const foods = await Food().findAll({
+      where: {
+        description: {
+          [Op.like]: `%${req.query.desc}%`,
+        },
+      },
+    })
+
+    res.status(200).json({ data: foods })
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching foods!' })
+  }
+})
 
 foodsRouter.get('/', async (req, res) => {
   try {
